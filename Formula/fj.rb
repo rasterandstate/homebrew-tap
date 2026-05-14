f303e6a98dda276265e664af4d8bf07dcbf342ea
Stephen Way <stephen@rasterstate.com>
2026-05-14 09:39:10 -0700

    Bump fj to 0.1.1 with new SHAs

--- Formula/fj.rb
+++ Formula/fj.rb
@@ -1,17 +1,17 @@
 class Fj < Formula
   desc "Command-line tool for Forgejo, in the spirit of gh"
   homepage "https://rasterhub.com/rasterstate/fj"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://rasterhub.com/rasterstate/fj/releases/download/v#{version}/fj-v#{version}-darwin-aarch64.tar.gz"
-      sha256 "eb5fd0206a5194179f6d1aecee7fe4a4a5434e1f44ed75a525f7f41d18670061"
+      sha256 "f0e8f492f89e274f28f671ffd2bee86cc306435005b0bf6fd03c19aed453af53"
     end
     on_intel do
       url "https://rasterhub.com/rasterstate/fj/releases/download/v#{version}/fj-v#{version}-darwin-x86_64.tar.gz"
-      sha256 "d7102095f051f4af7af1d9dbf34678df85142fdfece71dcab0cc111aeb942c2c"
+      sha256 "06505e7820b9e7f712bb21c55da2ceb2be26a9f35b1aa47ea7d09c99d15eb28a"
     end
   end
 
